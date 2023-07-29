1b37c45b617ada59663602e719f36a64481cdb5a
Nishant Gurjar <118594124+nishantgurjar012@users.noreply.github.com>
2023-07-29 23:54:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ function fibonacci(num) {
 }
 
 // Test the function
-console.log(fibonacci(0)); // Output: 0
-console.log(fibonacci(1)); // Output: 1
-console.log(fibonacci(2)); // Output: 1
-console.log(fibonacci(3)); // Output: 2
-console.log(fibonacci(4)); // Output: 3
-console.log(fibonacci(5)); // Output: 5
+// console.log(fibonacci(0)); // Output: 0
+// console.log(fibonacci(1)); // Output: 1
+// console.log(fibonacci(2)); // Output: 1
+// console.log(fibonacci(3)); // Output: 2
+// console.log(fibonacci(4)); // Output: 3
+// console.log(fibonacci(5)); // Output: 5
 // and so on...
